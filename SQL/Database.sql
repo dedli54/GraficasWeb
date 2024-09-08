479e28aebf7be36c55927fe2ed08c5a1a7bbf8db
dedli54 <betogarciacamacho@gmail.com>
2024-09-08 14:54:09 -0600

    Update de la database

--- SQL/Database.sql
+++ SQL/Database.sql
@@ -5,11 +5,12 @@
 
 
 CREATE TABLE loged (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(255) NOT NULL,
-    contrasena VARCHAR(255) NOT NULL,
-    correo VARCHAR(255) NOT NULL,
-    fechaNacimiento DATE NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY, -- Unique identifier for each user
+    usuario VARCHAR(255) NOT NULL, -- Username of the user
+    contrasena VARCHAR(255) NOT NULL, -- Password of the user
+    correo VARCHAR(255) NOT NULL, -- Email address of the user
+    fechaNacimiento DATE NOT NULL, -- Birthdate of the user
+    puntuacion INT NOT NULL -- Score of the user
 );
 
 SELECT * FROM loged;
