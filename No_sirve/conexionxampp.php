32b35db34a19ab14bf2ef0eec08066801477b179
Alberto Adrian Garcia Camacho <betogarciacamacho@gmail.com>
2024-09-09 17:01:56 -0600

    No hay que hacer merge de esta branch, nomas es para la primera entrega

--- No_sirve/conexionxampp.php
+++
@@ -1,9 +0,0 @@
-<?php
-//$dbhost="localhost";
-//$dbuser="root";
-//$dbpass="Admin123";
-//$dbname="grafica";
-
-if(!$con=mysqli_connect("localhost", "root", "", "grafica")){
-    die("failed to connect");
-}
